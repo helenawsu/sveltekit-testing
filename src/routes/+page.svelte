c9a802ba9e49a0b6a73b0bd22f4bc81185f92444
Helena Su <helenasu@berkeley.edu>
2023-07-06 16:16:10 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    const refresh = () => {
+        alert("Hello\nHow are you?");
+
+    }
+</script>
+<h1>Welcome to SvelteKit</h1>
+<button on:click={refresh}>click to alert</button>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
